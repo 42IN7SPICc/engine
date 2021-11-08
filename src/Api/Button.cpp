6e8326ecf6da82695c705a89949f318ad4619096
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-08 16:31:49 +0100

    Create Button.cpp

---
+++ src/Api/Button.cpp
@@ -0,0 +1,14 @@
+#include "Button.hpp"
+#include "Input.hpp"
+
+#include "../Listeners/ButtonMouseListener.hpp"
+
+using namespace engine;
+using namespace spic;
+
+Button::Button(const std::string& name, const std::string& tag, int layer, double width, double height) : UIObject(name, tag, layer, width, height) {
+    interactable = true;
+
+    ButtonMouseListener listener{this};
+    Input::RegisterMouseListener(&listener);
+}
