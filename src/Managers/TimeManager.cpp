078c3fa1e9a4dee59122521bdf3ff5811fe5338d
daanh432 <daan@daanhendriks.nl>
2021-11-08 16:14:31 +0100

    Implemented the time manager and improved the include statements

---
+++ src/Managers/TimeManager.cpp
@@ -0,0 +1,44 @@
+#include "TimeManager.hpp"
+#include <iostream>
+#include "SDL_timer.h"
+
+using namespace engine;
+
+const double MAX_DELTA_TIME = 200.0;
+TimeManager TimeManager::_instance{};
+
+double TimeManager::DeltaTime() const {
+    return _deltaTime * 1000;
+}
+
+double TimeManager::TimeScale() const {
+    return _timeScale;
+}
+
+void TimeManager::TimeScale(double newTimeScale) {
+    _timeScale = newTimeScale;
+}
+
+void TimeManager::Update() {
+    _lastTick = _currentTick;
+    _currentTick = SDL_GetPerformanceCounter();
+
+    if (_lastTick == -1) {
+        _lastTick = _currentTick;
+    }
+
+    auto deltaTime = (double) ((_currentTick - _lastTick) * 1000 / (double) SDL_GetPerformanceFrequency()); // NOLINT(cppcoreguidelines-narrowing-conversions)
+
+    if (deltaTime > MAX_DELTA_TIME) {
+#ifndef NDEBUG
+        std::cout << "Can't keep up! Did the system time change, or is the application overloaded? Running " << deltaTime << "ms behind, skipping ticks" << std::endl;
+#endif
+        deltaTime = MAX_DELTA_TIME;
+    }
+
+    _deltaTime = deltaTime;
+}
+
+TimeManager &TimeManager::GetInstance() {
+    return _instance;
+}
