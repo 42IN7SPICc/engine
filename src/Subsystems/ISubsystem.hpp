2bc0d804fe2608b328375887192b7c766feb7e50
daanh432 <daan@daanhendriks.nl>
2021-11-08 16:14:38 +0100

    Subsystem interface

---
+++ src/Subsystems/ISubsystem.hpp
@@ -0,0 +1,12 @@
+#ifndef SPIC_ENGINE_ISUBSYSTEM_HPP
+#define SPIC_ENGINE_ISUBSYSTEM_HPP
+
+namespace engine {
+    class ISubsystem {
+    public:
+        virtual void Update() = 0;
+    };
+}
+
+
+#endif //SPIC_ENGINE_ISUBSYSTEM_HPP
