7ef898c0d3a6b99d21fa40cdb3b0e8f6ad350a6d
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-09 15:29:50 +0100

    Update Engine.cpp

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -24,9 +24,9 @@ Engine &Engine::Instance() {
 
 void Engine::Start() {
     std::vector<std::shared_ptr<engine::ISubsystem>> subsystems = {
-            std::make_shared<engine::AnimatorSubsystem>(),
-            std::make_shared<engine::BehaviourScriptSubsystem>(),
             std::make_shared<engine::InputSubsystem>(),
+            std::make_shared<engine::BehaviourScriptSubsystem>(),
+            std::make_shared<engine::AnimatorSubsystem>(),
             std::make_shared<engine::RenderSubsystem>(),
     };
 
