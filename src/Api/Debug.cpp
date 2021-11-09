897f3ccdaeb4c356022143a275194a9dc10a87a7
Tommy Hosewol <t.hosewol@gmail.com>
2021-11-09 14:53:43 +0100

    Temporary revert for RFC

--- src/Api/Debug.cpp
+++ src/Api/Debug.cpp
@@ -8,14 +8,14 @@ void Debug::DrawLine(const Point& start, const Point& end, const Color& color) {
     // TODO: Cannot be implemented if the render subsystem is not done
 }
 
-void Debug::Log(const std::string& message, const std::string& caller) {
-    std::cout << "INFO|" << caller << "|" << message << std::endl;
+void Debug::Log(const std::string& message) {
+    std::cout << "INFO|" << message << std::endl;
 }
 
-void Debug::LogError(const std::string& error, const std::string& caller) {
-    std::cerr << "ERROR|" << caller << "|" << error << std::endl;
+void Debug::LogError(const std::string& error) {
+    std::cerr << "ERROR|" << error << std::endl;
 }
 
-void Debug::LogWarning(const std::string& warning, const std::string& caller) {
-    std::cerr << "WARNING|" << caller << "|" << warning << std::endl;
+void Debug::LogWarning(const std::string& warning) {
+    std::cerr << "WARNING|" << warning << std::endl;
 }
