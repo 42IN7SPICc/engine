08470b1398b6b84e2291b1bc60ee2557f40c4a66
MaxvdVossenberg <62118062+MaxvdVossenberg@users.noreply.github.com>
2021-11-09 15:06:04 +0100

    Update src/Api/CircleCollider.cpp
    Co-authored-by: Sander Jochems <sanderjochems@hotmail.nl>

--- src/Api/CircleCollider.cpp
+++ src/Api/CircleCollider.cpp
@@ -2,8 +2,7 @@
 
 using namespace spic;
 
-CircleCollider::CircleCollider() {
-    this->radius = 0;
+CircleCollider::CircleCollider() : CircleCollider(0) {
 }
 
 CircleCollider::CircleCollider(double radius) : radius(radius) {}
