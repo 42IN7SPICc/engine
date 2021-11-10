e11a1bbce69b65cb667c710191597693d708bbde
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-11-10 10:11:51 +0100

    Removed testing code from main and reordered subsystems

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -26,9 +26,9 @@ void Engine::Start() {
     std::vector<std::shared_ptr<engine::ISubsystem>> subsystems = {
             std::make_shared<engine::InputSubsystem>(),
             std::make_shared<engine::BehaviourScriptSubsystem>(),
+            std::make_shared<engine::PhysicsSubsystem>(),
             std::make_shared<engine::AnimatorSubsystem>(),
             std::make_shared<engine::RenderSubsystem>(),
-            std::make_shared<engine::PhysicsSubsystem>()
     };
     _running = true;
 
