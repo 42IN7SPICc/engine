60cc3192748d5a5b17d7003bfcabb2009c04c936
Tommy Hosewol <t.hosewol@gmail.com>
2021-11-10 11:04:57 +0100

    Finish logging

--- src/Api/Debug.cpp
+++ src/Api/Debug.cpp
@@ -8,14 +8,14 @@ void Debug::DrawLine(const Point& start, const Point& end, const Color& color) {
     // TODO: Cannot be implemented if the render subsystem is not done
 }
 
-void Debug::Log(const std::string& message) {
-    std::cout << "INFO|" << message << std::endl;
+void Debug::Log(const std::string& message, const std::string& caller,  const std::string& file, const int& line) {
+    std::cout << "INFO|" << caller << "|" << file << ":" << line << "|" << message << std::endl;
 }
 
-void Debug::LogError(const std::string& error) {
-    std::cerr << "ERROR|" << error << std::endl;
+void Debug::LogError(const std::string& error, const std::string& caller,  const std::string& file, const int& line) {
+    std::cerr << "ERROR|" << caller << "|" << file << ":" << line << "|" << error << std::endl;
 }
 
-void Debug::LogWarning(const std::string& warning) {
-    std::cerr << "WARNING|" << warning << std::endl;
+void Debug::LogWarning(const std::string& warning, const std::string& caller,  const std::string& file, const int& line) {
+    std::cerr << "WARNING|" << caller << "|" << file << ":" << line << "|" << warning << std::endl;
 }
