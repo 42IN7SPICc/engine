58ef3b2a304c4805d1578fe8bf832065652d4807
Tommy Hosewol <t.hosewol@gmail.com>
2021-11-10 11:07:52 +0100

    Spaces for the boys

--- src/Api/Debug.cpp
+++ src/Api/Debug.cpp
@@ -9,13 +9,13 @@ void Debug::DrawLine(const Point& start, const Point& end, const Color& color) {
 }
 
 void Debug::Log(const std::string& message, const std::string& caller,  const std::string& file, const int& line) {
-    std::cout << "INFO|" << caller << "|" << file << ":" << line << "|" << message << std::endl;
+    std::cout << "INFO | " << caller << " | " << file << ":" << line << " | " << message << std::endl;
 }
 
 void Debug::LogError(const std::string& error, const std::string& caller,  const std::string& file, const int& line) {
-    std::cerr << "ERROR|" << caller << "|" << file << ":" << line << "|" << error << std::endl;
+    std::cerr << "ERROR | " << caller << " | " << file << ":" << line << " | " << error << std::endl;
 }
 
 void Debug::LogWarning(const std::string& warning, const std::string& caller,  const std::string& file, const int& line) {
-    std::cerr << "WARNING|" << caller << "|" << file << ":" << line << "|" << warning << std::endl;
+    std::cerr << "WARNING | " << caller << " | " << file << ":" << line << " | " << warning << std::endl;
 }
