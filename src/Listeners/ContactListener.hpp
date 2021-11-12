19ce83a4600b9f195e2a89a4082682815670389c
daanh432 <daan@daanhendriks.nl>
2021-11-12 15:53:25 +0100

    Contact listener support for trigger stay

--- src/Listeners/ContactListener.hpp
+++ src/Listeners/ContactListener.hpp
@@ -3,14 +3,21 @@
 
 #include <box2d/box2d.h>
 #include <Component.hpp>
+#include <map>
+#include <BehaviourScript.hpp>
+#include <vector>
 
 class ContactListener : public b2ContactListener {
 public:
+    void StartNewPhysicsSession();
+
     void BeginContact(b2Contact *contact) override;
 
     void EndContact(b2Contact *contact) override;
 private:
-    static bool GetEntities(b2Contact *contact, spic::Component *&entityA, spic::Component *&entityB) ;
+    std::map<std::shared_ptr<spic::BehaviourScript>, std::vector<std::shared_ptr<spic::Collider>>> _currentCollisions = {};
+    std::map<std::shared_ptr<spic::BehaviourScript>, std::vector<std::shared_ptr<spic::Collider>>> _previousCollisions = {};
+    static bool GetEntities(b2Contact *contact, spic::Component *&entityA, spic::Component *&entityB);
 };
 
 
