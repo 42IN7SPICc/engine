d3951d76f1311b3889f5b58283553e42fe4ec2eb
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-12 14:55:31 +0100

    Update Input.cpp

--- src/Api/Input.cpp
+++ src/Api/Input.cpp
@@ -2,59 +2,75 @@
 
 #include "../Managers/InputManager.hpp"
 
-#include <algorithm>
-
 using namespace spic;
 using namespace engine;
 
-bool Input::AnyKey() {
+bool Input::AnyKey()
+{
     return InputManager::GetInstance().AnyKey();
 }
 
-bool Input::AnyKeyDown() {
+bool Input::AnyKeyDown()
+{
     return InputManager::GetInstance().AnyKeyDown();
 }
 
-const Point Input::MousePosition() {
+const Point Input::MousePosition()
+{
     return InputManager::GetInstance().MousePosition();
 }
 
-bool Input::GetKey(KeyCode key) {
+bool Input::GetKey(KeyCode key)
+{
     return InputManager::GetInstance().KeyDown(key);
 }
 
-bool Input::GetKeyDown(KeyCode key) {
+bool Input::GetKeyDown(KeyCode key)
+{
     return InputManager::GetInstance().KeyFrameDown(key);
 }
 
-bool Input::GetKeyUp(KeyCode key) {
+bool Input::GetKeyUp(KeyCode key)
+{
     return InputManager::GetInstance().KeyFrameUp(key);
 }
 
-bool Input::GetMouseButton(MouseButton which) {
+bool Input::GetMouseButton(MouseButton which)
+{
     return InputManager::GetInstance().MouseDown(which);
 }
 
-bool Input::GetMouseButtonDown(MouseButton which) {
+bool Input::GetMouseButtonDown(MouseButton which)
+{
     return InputManager::GetInstance().MouseFrameDown(which);
 }
 
-bool Input::GetMouseButtonUp(MouseButton which) {
+bool Input::GetMouseButtonUp(MouseButton which)
+{
     return InputManager::GetInstance().MouseFrameUp(which);
 }
 
-void Input::RegisterKeyListener(IKeyListener& listener) {
-    _keyListeners.push_back(&listener);
+double Input::GetAxis()
+{
+    return 0; // TODO
 }
 
-void Input::UnregisterKeyListener(IKeyListener& listener) {
-    std::remove(_keyListeners.begin(), _keyListeners.end(), &listener);
+void Input::RegisterKeyListener(IKeyListener& listener)
+{
+    InputManager::GetInstance().RegisterKeyListener(listener);
 }
 
-void Input::RegisterMouseListener(IMouseListener& listener) {
-    _mouseListeners.push_back(&listener);
+void Input::UnregisterKeyListener(IKeyListener& listener)
+{
+    InputManager::GetInstance().UnregisterKeyListener(listener);
 }
 
-void Input::UnregisterMouseListener(IMouseListener& listener) {
-    std::remove(_mouseListeners.begin(), _mouseListeners.end(), &listener);
+void Input::RegisterMouseListener(IMouseListener& listener)
+{
+    InputManager::GetInstance().RegisterMouseListener(listener);
+}
+
+void Input::UnregisterMouseListener(IMouseListener& listener)
+{
+    InputManager::GetInstance().UnregisterMouseListener(listener);
 }
