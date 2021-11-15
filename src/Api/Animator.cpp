a7913e2db8b795f98f0a86cde15313d8b5934da3
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-15 13:20:59 +0100

    Update Animator.cpp

--- src/Api/Animator.cpp
+++ src/Api/Animator.cpp
@@ -5,16 +5,26 @@
 
 using namespace spic;
 
-Animator::Animator(int fps, const std::vector<std::shared_ptr<Sprite>> &sprites) : fps{fps}, _sprites{std::move(sprites)} {
+Animator::Animator(int fps, const std::vector<std::shared_ptr<Sprite>>& sprites) : fps{fps},
+                                                                                   _sprites{std::move(sprites)},
+                                                                                   Playing{false},
+                                                                                   Looping{false},
+                                                                                   CurrentFrame{0},
+                                                                                   FrameTime{0}
+{
     FrameDuration = 1.0 / fps;
 }
 
-void Animator::ActivateFrame(int frameIndex) {
-    if (frameIndex >= 0 && frameIndex < Frames()) {
+void Animator::ActivateFrame(int frameIndex)
+{
+    if (frameIndex >= 0 && frameIndex < Frames())
+    {
         auto gameObject = GameObject();
-        if (!gameObject.expired()) {
+        if (!gameObject.expired())
+        {
             auto sprite = gameObject.lock()->GetComponent<Sprite>();
-            if (sprite) {
+            if (sprite)
+            {
                 auto frameSprite = _sprites[frameIndex];
 
                 sprite->Texture(frameSprite->Texture());
@@ -26,23 +36,28 @@ void Animator::ActivateFrame(int frameIndex) {
     }
 }
 
-int Animator::Frames() {
+int Animator::Frames()
+{
     return _sprites.size();
 }
 
-int Animator::FPS() const {
+int Animator::FPS() const
+{
     return fps;
 }
 
-void Animator::FPS(int newFps) {
+void Animator::FPS(int newFps)
+{
     fps = newFps;
 }
 
-void Animator::Play(bool looping) {
+void Animator::Play(bool looping)
+{
     Playing = true;
     Looping = looping;
 }
 
-void Animator::Stop() {
+void Animator::Stop()
+{
     Playing = false;
 }
