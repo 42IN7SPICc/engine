257d0fe621cd621953c93fd393ba5d9f0a517d75
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-15 13:38:41 +0100

    Update GameObject.cpp

--- src/Api/GameObject.cpp
+++ src/Api/GameObject.cpp
@@ -6,8 +6,10 @@
 
 using namespace spic;
 
-void spic_GameObject_ListObjects(const std::shared_ptr<GameObject> gameObject, std::vector<std::shared_ptr<GameObject>> &gameObjects, bool includeInactive = false) {
-    if (includeInactive || gameObject->Active()) {
+void spic_GameObject_ListObjects(const std::shared_ptr<GameObject> gameObject, std::vector<std::shared_ptr<GameObject>>& gameObjects, bool includeInactive = false)
+{
+    if (includeInactive || gameObject->Active())
+    {
         gameObjects.push_back(gameObject);
 
         for (const auto& childObject: gameObject->Children())
@@ -17,22 +19,27 @@ void spic_GameObject_ListObjects(const std::shared_ptr<GameObject> gameObject, s
     }
 }
 
-std::vector<std::shared_ptr<GameObject>> GameObject::All(bool includeInactive) {
+std::vector<std::shared_ptr<GameObject>> GameObject::All(bool includeInactive)
+{
     std::vector<std::shared_ptr<GameObject>> gameObjects{};
     auto scene = Engine::Instance().PeekScene();
 
-    for (const auto &gameObject: scene->Contents()) {
+    for (const auto& gameObject: scene->Contents())
+    {
         spic_GameObject_ListObjects(gameObject, gameObjects, includeInactive);
     }
 
     return gameObjects;
 }
 
-std::vector<std::shared_ptr<GameObject>> GameObject::FindGameObjectsWithTag(const std::string &tag) {
+std::vector<std::shared_ptr<GameObject>> GameObject::FindGameObjectsWithTag(const std::string& tag)
+{
     std::vector<std::shared_ptr<GameObject>> list{};
 
-    for (auto obj: All()) {
-        if (obj->tag == tag && obj->Active()) {
+    for (auto obj: All())
+    {
+        if (obj->tag == tag && obj->Active())
+        {
             list.push_back(obj);
         }
     }
@@ -40,9 +47,12 @@ std::vector<std::shared_ptr<GameObject>> GameObject::FindGameObjectsWithTag(cons
     return list;
 }
 
-std::shared_ptr<GameObject> GameObject::FindWithTag(const std::string &tag) {
-    for (auto obj: All()) {
-        if (obj->tag == tag && obj->Active()) {
+std::shared_ptr<GameObject> GameObject::FindWithTag(const std::string& tag)
+{
+    for (auto obj: All())
+    {
+        if (obj->tag == tag && obj->Active())
+        {
             return obj;
         }
     }
@@ -50,105 +60,138 @@ std::shared_ptr<GameObject> GameObject::FindWithTag(const std::string &tag) {
     return nullptr;
 }
 
-void GameObject::Destroy(std::shared_ptr<GameObject> obj) {
+void GameObject::Destroy(std::shared_ptr<GameObject> obj)
+{
     auto scene = Engine::Instance().PeekScene();
 
-    for (auto object: All()) {
-        if (object->name == obj->name) {
+    for (auto object: All())
+    {
+        if (object->name == obj->name)
+        {
             scene->Contents().erase(std::remove(scene->Contents().begin(), scene->Contents().end(), obj), scene->Contents().end());
         }
     }
 }
 
-void GameObject::Destroy(Component *obj) {
+void GameObject::Destroy(Component* obj)
+{
     std::shared_ptr<Component> component(obj);
 
-    if (!component->GameObject().expired()) {
+    if (!component->GameObject().expired())
+    {
         component->GameObject().lock()->RemoveComponent(component);
     }
 }
 
-GameObject::GameObject(const std::string &name, const std::string &tag, int layer) : active(true), name{name}, tag{tag}, layer{layer} {
-    _transform = {{0, 0}, 0, 1};
+GameObject::GameObject(const std::string& name, const std::string& tag, int layer) : active{true},
+                                                                                     name{name},
+                                                                                     tag{tag},
+                                                                                     layer{layer},
+                                                                                     _parent{},
+                                                                                     _children{},
+                                                                                     _components{},
+                                                                                     _transform{{0, 0}, 0, 1}
+{
 }
 
-GameObject::operator bool() const {
+GameObject::operator bool() const
+{
     return IsActiveInWorld();
 }
 
-bool GameObject::operator!=(const GameObject &other) const {
+bool GameObject::operator!=(const GameObject& other) const
+{
     return name != other.name;
 }
 
-bool GameObject::operator==(const GameObject &other) const {
+bool GameObject::operator==(const GameObject& other) const
+{
     return name == other.name;
 }
 
-bool GameObject::Active() const {
+bool GameObject::Active() const
+{
     return active;
 }
 
-void GameObject::Active(bool flag) {
+void GameObject::Active(bool flag)
+{
     active = flag;
 }
 
-bool GameObject::IsActiveInWorld() const {
-    if (!_parent.expired()) {
+bool GameObject::IsActiveInWorld() const
+{
+    if (!_parent.expired())
+    {
         return this->Active() && _parent.lock()->IsActiveInWorld();
     }
     return this->Active();
 }
 
-const spic::Transform &GameObject::Transform() const {
+const spic::Transform& GameObject::Transform() const
+{
     return _transform;
 }
 
-spic::Transform &GameObject::Transform() {
+spic::Transform& GameObject::Transform()
+{
     return _transform;
 }
 
-std::weak_ptr<GameObject> GameObject::Parent() const {
+std::weak_ptr<GameObject> GameObject::Parent() const
+{
     return _parent;
 }
 
-void GameObject::Parent(std::weak_ptr<GameObject> parent) {
+void GameObject::Parent(std::weak_ptr<GameObject> parent)
+{
     _parent = std::move(parent);
 }
 
-const std::vector<std::shared_ptr<GameObject>> &GameObject::Children() const {
+const std::vector<std::shared_ptr<GameObject>>& GameObject::Children() const
+{
     return _children;
 }
 
-void GameObject::AddChild(std::shared_ptr<GameObject> child) {
-    if (std::find(_children.begin(), _children.end(), child) == _children.end()) {
+void GameObject::AddChild(std::shared_ptr<GameObject> child)
+{
+    if (std::find(_children.begin(), _children.end(), child) == _children.end())
+    {
         _children.push_back(child);
     }
 }
 
-void GameObject::RemoveChild(std::shared_ptr<GameObject> child) {
-    if (std::find(_children.begin(), _children.end(), child) == _children.end()) {
+void GameObject::RemoveChild(std::shared_ptr<GameObject> child)
+{
+    if (std::find(_children.begin(), _children.end(), child) == _children.end())
+    {
         _children.erase(std::remove(_children.begin(), _children.end(), child), _children.end());
         child->_parent.reset();
     }
 }
 
-const std::string &GameObject::Name() const {
+const std::string& GameObject::Name() const
+{
     return name;
 }
 
-const std::string &GameObject::Tag() const {
+const std::string& GameObject::Tag() const
+{
     return tag;
 }
 
-int GameObject::Layer() const {
+int GameObject::Layer() const
+{
     return layer;
 }
 
-spic::Transform GameObject::AbsoluteTransform() const {
-    const GameObject *object = this;
+spic::Transform GameObject::AbsoluteTransform() const
+{
+    const GameObject* object = this;
     spic::Transform transform{object->Transform()};
 
-    while (!object->Parent().expired()) {
+    while (!object->Parent().expired())
+    {
         object = object->Parent().lock().get();
         transform.position.x += object->Transform().position.x;
         transform.position.y += object->Transform().position.y;
@@ -161,6 +204,7 @@ spic::Transform GameObject::AbsoluteTransform() const {
     return transform;
 }
 
-void GameObject::RemoveComponent(std::shared_ptr<Component> component) {
+void GameObject::RemoveComponent(std::shared_ptr<Component> component)
+{
     _components.erase(std::remove(_components.begin(), _components.end(), component), _components.end());
 }
