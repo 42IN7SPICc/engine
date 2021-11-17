43eec55f7fb2a42611a63d8a6059f5a3b28491d9
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-17 16:27:22 +0100

    Update BehaviourScriptSubsystem.cpp

--- src/Subsystems/BehaviourScriptSubsystem.cpp
+++ src/Subsystems/BehaviourScriptSubsystem.cpp
@@ -1,24 +1,33 @@
 #include "BehaviourScriptSubsystem.hpp"
 
 #include "BehaviourScript.hpp"
+#include "Debug.hpp"
 #include "GameObject.hpp"
 
 using namespace engine;
 using namespace spic;
 
-void BehaviourScriptSubsystem::Update() {
+void BehaviourScriptSubsystem::Update()
+{
     for (const std::shared_ptr<GameObject>& gameObject: GameObject::All())
     {
         for (const std::shared_ptr<BehaviourScript>& behaviourScript: gameObject->GetComponents<BehaviourScript>())
         {
             if (behaviourScript->Active())
             {
-                if (!behaviourScript->Started())
+                if (!behaviourScript->GameObject().expired())
                 {
-                    behaviourScript->Started(true);
-                    behaviourScript->OnStart();
+                    if (!behaviourScript->Started())
+                    {
+                        behaviourScript->Started(true);
+                        behaviourScript->OnStart();
+                    }
+                    behaviourScript->OnUpdate();
                 }
-                behaviourScript->OnUpdate();
+                else
+                {
+                    Debug::LogWarning("BehaviourScript does not contains a GameObject.");
+                }
             }
         }
     }
