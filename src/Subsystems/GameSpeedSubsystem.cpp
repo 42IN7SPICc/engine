a2c4b8f8e0689415a27bc7b489bfb36769997e6d
daanh432 <daan@daanhendriks.nl>
2021-11-20 13:40:19 +0100

    Added some debug warnings when timescale goes out of bounds

--- src/Subsystems/GameSpeedSubsystem.cpp
+++ src/Subsystems/GameSpeedSubsystem.cpp
@@ -1,6 +1,7 @@
 #include "GameSpeedSubsystem.hpp"
 #include <Input.hpp>
 #include <Time.hpp>
+#include <Debug.hpp>
 
 #define MAX_TIME_SCALE 2.0
 #define MIN_TIME_SCALE 0.5
@@ -13,12 +14,16 @@ void engine::GameSpeedSubsystem::Update()
         auto timeScale = spic::Time::TimeScale();
         if (timeScale < MAX_TIME_SCALE)
             spic::Time::TimeScale(timeScale + TIME_SCALE_STEP);
+        else
+            spic::Debug::LogWarning("Maximum time scale has been reached. Not increasing time scale");
     }
     else if (spic::Input::GetKeyDown(spic::Input::KeyCode::PAGE_DOWN))
     {
         auto timeScale = spic::Time::TimeScale();
         if (timeScale > MIN_TIME_SCALE)
             spic::Time::TimeScale(timeScale - TIME_SCALE_STEP);
+        else
+            spic::Debug::LogWarning("Minimum time scale has been reached. Not decreasing time scale");
     }
     else if (spic::Input::GetKeyDown(spic::Input::KeyCode::HOME))
     {
