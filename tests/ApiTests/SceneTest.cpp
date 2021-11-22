8a5e5846756f69dead4bc6f54dea5595b8b19aa8
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-11-22 17:11:45 +0100

    Add Scene Test

--- tests/ApiTests/SceneTest.cpp
+++ tests/ApiTests/SceneTest.cpp
@@ -1,2 +1,12 @@
 #include <gtest/gtest.h>
+#include "Scene.hpp"
+#include "GameObject.hpp"
+
+spic::Scene scene {};
+
+TEST(SceneTest, TestContentProperty) {
+    scene.Contents().push_back(std::make_shared<spic::GameObject>("test1", "test", 0));
+    scene.Contents().push_back(std::make_shared<spic::GameObject>("test2", "test", 0));
+    ASSERT_EQ(2, scene.Contents().size());
+}
 
