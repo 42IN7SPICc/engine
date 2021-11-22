3efa3deeeb1ffc09daabb5f64a0430d2a884387b
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-11-22 15:48:26 +0100

    Added Debug Test

---
+++ tests/ApiTests/DebugTest.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "Debug.hpp"
+#include "string"
+
+TEST(DebugTest, TestLog) {
+    testing::internal::CaptureStdout();
+    spic::Debug::Log("basic test log");
+    std::string output = testing::internal::GetCapturedStdout();
+    ASSERT_THAT(output, ::testing::HasSubstr("basic test log"));
+    ASSERT_THAT(output, ::testing::HasSubstr("INFO"));
+}
+
+TEST(DebugTest, TestLogError) {
+    testing::internal::CaptureStderr();
+    spic::Debug::LogError("basic test error");
+    std::string output = testing::internal::GetCapturedStderr();
+    ASSERT_THAT(output, ::testing::HasSubstr("basic test error"));
+    ASSERT_THAT(output, ::testing::HasSubstr("ERROR"));
+}
+
+TEST(DebugTest, TestLogWarning) {
+    testing::internal::CaptureStderr();
+    spic::Debug::LogWarning("basic test warn");
+    std::string output = testing::internal::GetCapturedStderr();
+    ASSERT_THAT(output, ::testing::HasSubstr("basic test warn"));
+    ASSERT_THAT(output, ::testing::HasSubstr("WARNING"));
+}
