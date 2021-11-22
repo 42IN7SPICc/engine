cd0a9684652653012165902811564e9f41e2b573
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-11-22 11:26:45 +0100

    Added Example Test for RigidBody (Full Property Implementation)

--- tests/ApiTests/RigidBodyTest.cpp
+++ tests/ApiTests/RigidBodyTest.cpp
@@ -1,23 +1,45 @@
 #include "RigidBody.hpp"
 #include <gtest/gtest.h>
 
-//RigidBody::RigidBody(double mass, double gravityScale, const BodyType& bodyType) : mass{mass},
-//                                                                                   gravityScale{gravityScale},
-//                                                                                   bodyType{bodyType},
-//                                                                                   _point(std::make_unique<spic::Point>())
-//{
-//}
-//BodyType RigidBody::Type() const;
-//void RigidBody::Type(BodyType newBodyType);
-//double RigidBody::Mass() const;
-//void RigidBody::Mass(double newMass);
-//double RigidBody::GravityScale() const;
-//void RigidBody::GravityScale(double newGravityScale);
-//void RigidBody::AddForce(const spic::Point& forceDirection);
-//void RigidBody::Point(const spic::Point& point);
+spic::RigidBody rigidBody {0,0, spic::BodyType::dynamicBody};
 
+TEST(RigidBodyTest, TestBodyProperty) {
+    rigidBody.Type(spic::BodyType::staticBody);
+    ASSERT_EQ(spic::BodyType::staticBody, rigidBody.Type());
+}
 
-TEST(RigidBodyTest, RigidBodyType) {
-    spic::RigidBody body {100, 25, spic::BodyType::dynamicBody};
-    ASSERT_EQ(spic::BodyType::dynamicBody , body.Type());
+TEST(RigidBodyTest, TestMassProperty) {
+    rigidBody.Mass(25.5);
+    ASSERT_EQ(25.5, rigidBody.Mass());
+}
+
+TEST(RigidBodyTest, TestGravityScaleProperty) {
+    rigidBody.GravityScale(33.9);
+    ASSERT_EQ(33.9, rigidBody.GravityScale());
+}
+
+TEST(RigidBodyTest, TestPointProperty) {
+    rigidBody.Point({99.9, 1.12});
+    ASSERT_EQ(99.9, rigidBody.Point().x);
+    ASSERT_EQ(1.12, rigidBody.Point().y);
+}
+
+TEST(RigidBodyTest, TestAddForceMethodPositive) {
+    rigidBody.Point({0.0, 0.0});
+    rigidBody.AddForce({5.5, 2.5});
+    ASSERT_EQ(5.5, rigidBody.Point().x);
+    ASSERT_EQ(2.5, rigidBody.Point().y);
+    rigidBody.AddForce({5.5, 2.5});
+    ASSERT_EQ(11.0, rigidBody.Point().x);
+    ASSERT_EQ(5.0,rigidBody.Point().y);
+}
+
+TEST(RigidBodyTest, TestAddForceMethodNegative) {
+    rigidBody.Point({10.0, 11.0});
+    rigidBody.AddForce({-5.5, -2.5});
+    ASSERT_EQ(4.5, rigidBody.Point().x);
+    ASSERT_EQ(8.5, rigidBody.Point().y);
+    rigidBody.AddForce({-3.5, -9});
+    ASSERT_EQ(1.0, rigidBody.Point().x);
+    ASSERT_EQ(-0.5,rigidBody.Point().y);
 }
