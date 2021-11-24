e55685f04249835d45da0d0a7d41661f01f5f409
Tommy Hosewol <t.hosewol@gmail.com>
2021-11-24 16:52:01 +0100

    Update JsonFacade_templates.hpp

--- includes/Engine/JsonFacade_templates.hpp
+++ includes/Engine/JsonFacade_templates.hpp
@@ -1,5 +1,11 @@
 #include "JsonFacade.hpp"
+
 #include <cereal/archives/json.hpp>
+#include <cereal/types/map.hpp>
+#include <cereal/types/vector.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/complex.hpp>
+
 #include <sstream>
 
 template<class T>
