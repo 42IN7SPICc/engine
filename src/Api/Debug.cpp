fc4162842676177a743ea5111fbbc2267d6caa90
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-28 10:51:42 +0100

    Update Debug.cpp

--- src/Api/Debug.cpp
+++ src/Api/Debug.cpp
@@ -11,7 +11,7 @@ void Debug::DrawLine(const Point& start, const Point& end, const Color& color)
 
 void Debug::Log(const std::string& message, const std::string& caller, const std::string& file, int line)
 {
-    std::cout << "INFO | " << caller << " | " << file << ":" << line << " | " << message << std::endl;
+    std::cout << "INFO | " << caller << " | " << file << ":" << line << " | " << message << "\n";
 }
 
 void Debug::LogError(const std::string& error, const std::string& caller, const std::string& file, int line)
