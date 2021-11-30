6b32cfb5948c0347357da17dc0995e81a5d76d72
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-30 17:38:31 +0100

    Add comments for managers

--- src/Managers/AudioManager.hpp
+++ src/Managers/AudioManager.hpp
@@ -8,6 +8,9 @@
 
 namespace engine
 {
+    /**
+     * @brief Caches multiple audio clips to prevent the Engine from accessing files multiple times.
+     */
     class AudioManager
     {
         private:
@@ -15,16 +18,45 @@ namespace engine
             std::map<std::string, std::shared_ptr<AudioClip>> _audioClips = {};
             std::map<std::string, int> _channels = {};
         public:
+            /**
+             * @brief Triggers when a Channel has been finished and needs to be cleared.
+             * @param channelNumber The number of the channel to be cleared.
+             */
             static void ChannelCallback(int channelNumber);
 
+            /**
+             * @brief Load a AudioClip from the filesystem.
+             * @param path The path to the audio file.
+             */
             void LoadAudioClip(const std::string& path);
 
+            /**
+             * @brief Checks whether a AudioClip has been chached for a given file.
+             * @param path The path to the audio file.
+             * @return Whether there is a cache item for the path.
+             */
             bool Contains(const std::string& path) const;
 
+            /**
+             * @brief Plays a AudioClip on a given GameObject.
+             * @param gameObjectName The name of the GameObject.
+             * @param path The path to the audio file.
+             * @param loop Whether to play the AudioClip in a loop.
+             * @param volume The volume to play the AudioClip at.
+             */
             void Play(const std::string& gameObjectName, const std::string& path, bool loop = false, double volume = 0.5);
 
+            /**
+             * @brief Stops a AudioClip from playing at a given GameObject.
+             * @param gameObjectName The name of the GameObject.
+             * @param path The path to the audio file.
+             */
             void Stop(const std::string& gameObjectName, const std::string& path);
 
+            /**
+             * @brief Get the instance of the AudioManager.
+             * @return The instance of the AudioManager.
+             */
             static AudioManager& GetInstance();
     };
 }
--- src/Managers/TimeManager.hpp
+++ src/Managers/TimeManager.hpp
@@ -3,30 +3,55 @@
 
 #include "../Models/Window.hpp"
 
-namespace engine {
+namespace engine
+{
+    /**
+     * @brief Stores the current DeltaTime and TimeScale.
+     */
+    class TimeManager
+    {
+        public:
+            /**
+             * @brief Constructs a new instance of a TimeManager.
+             */
+            TimeManager() = default;
 
-    class TimeManager {
-    public:
-        TimeManager() = default;
+            /**
+             * @brief Access the DeltaTime for the current frame.
+             * @return The delta time of the frame.
+             */
+            [[nodiscard]] double DeltaTime() const;
 
-        [[nodiscard]] double DeltaTime() const;
+            /**
+             * @brief Access the TimeScale of the Engine.
+             * @return The current time scale.
+             */
+            [[nodiscard]] double TimeScale() const;
 
-        [[nodiscard]] double TimeScale() const;
+            /**
+             * @brief Set the DeltaTime for the next frame.
+             * @param newTimeScale the new delta time.
+             */
+            void TimeScale(double newTimeScale);
 
-        void TimeScale(double newTimeScale);
+            /**
+             * @brief Calculate the delta time and set it for the next frame.
+             */
+            void Update();
 
-        void Update();
+            /**
+             * @brief Get the instance of the TimeManager.
+             * @return
+             */
+            static TimeManager& GetInstance();
 
-        static TimeManager &GetInstance();
-
-    private:
-        static TimeManager _instance;
-        uint64_t _currentTick = -1;
-        uint64_t _lastTick = -1;
-        double _deltaTime = 0.0;
-        double _timeScale = 1.0;
+        private:
+            static TimeManager _instance;
+            uint64_t _currentTick = -1;
+            uint64_t _lastTick = -1;
+            double _deltaTime = 0.0;
+            double _timeScale = 1.0;
     };
 }
-
 
 #endif //SPIC_ENGINE_TIMEMANAGER_HPP
