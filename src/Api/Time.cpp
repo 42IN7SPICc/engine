c904ac3ffe872868f14c7023c4870dec841eb46c
Sander Jochems <sanderjochems@hotmail.nl>
2021-12-06 11:24:00 +0100

    Format Time.cpp

--- src/Api/Time.cpp
+++ src/Api/Time.cpp
@@ -4,19 +4,22 @@
 using namespace engine;
 using namespace spic;
 
-double Time::DeltaTime() {
+double Time::DeltaTime()
+{
     return TimeManager::GetInstance().DeltaTime();
 }
 
-double Time::TimeScale() {
+double Time::TimeScale()
+{
     return TimeManager::GetInstance().TimeScale();
 }
 
-void Time::DeltaTime(double newDeltaTime) {
-    // stub for now
+void Time::DeltaTime(double newDeltaTime)
+{
     // This will not be used by the engine, because the TimeManager handles the delta time of the game.
 }
 
-void Time::TimeScale(double newTimeScale) {
+void Time::TimeScale(double newTimeScale)
+{
     TimeManager::GetInstance().TimeScale(newTimeScale);
 }
