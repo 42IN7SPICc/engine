c23831b45f73e60a51d3d4164a29ae8680e9f69e
Sander Jochems <sanderjochems@hotmail.nl>
2021-12-08 11:37:03 +0100

    Add comments to Exceptions

--- src/Exceptions/EngineException.hpp
+++ src/Exceptions/EngineException.hpp
@@ -6,14 +6,26 @@
 
 namespace engine
 {
+    /**
+     * @brief An exception to throw when something errors in the engine.
+     */
     class EngineException : public std::exception
     {
         private:
             std::string _message;
 
         public:
+            /**
+             *
+             * @param message The message describing the error.
+             * @param innerMessage The inner message for developers (empty to disable).
+             */
             explicit EngineException(const std::string& message, const std::string& innerMessage = "");
 
+            /**
+             * @brief The what part of the exception.
+             * @return The message of the exception.
+             */
             const char* what() const throw()
             {
                 return _message.c_str();
